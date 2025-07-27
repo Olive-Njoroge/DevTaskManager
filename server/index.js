47d0dea3fdd72798dd6d0d2a438b0bdebd346b83
Olive Njoroge <njorogeolive001@gmail.com>
2025-07-27 10:57:36 +0300

    Finalized server side

--- server/index.js
+++ server/index.js
@@ -1,5 +1,23 @@
 const connectDB = require('./config/db');
 const dotenv = require('dotenv');
 dotenv.config();
+
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
 connectDB();
 
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//api endpoints
+app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/tasks', require('./routes/taskRoutes'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server Running on Port http://localhost/${PORT}`);
+});
