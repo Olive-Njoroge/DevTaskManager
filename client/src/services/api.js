50e216aa269b9614e8197ee65cba0198f9434b5d
Olive Njoroge <njorogeolive001@gmail.com>
2025-07-29 20:17:58 +0300

    added an env file to the client side

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api"
+    baseURL: import.meta.env.BACKEND_URI
 });
 
 //INTERCEPTOR... makes sure we attach JWT automatically
